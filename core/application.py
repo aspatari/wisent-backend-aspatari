4e1ee02983f394e6b66361934961ac83f7ce42a6
Artur Spatari <artur.spatari@gmail.com>
2020-08-31 16:08:16 +0300

    feat(core) Tune application with load_modules

--- core/application.py
+++ core/application.py
@@ -1,26 +1,32 @@
 from typing import Type
 
+from tornado.util import import_object
 from tornado.web import Application
 import logging
-
+from common.application import BaseApplication
 from common.handler import BaseJsonRequestHandler
 
 logger = logging.getLogger("app")
 
 
 class App(Application):
+    apps = ["apps.posts", 'apps.clients', "apps.socket"]
     handlers = []
+    models = []
 
     def __init__(self):
         settings = {"debug": True}
-        from posts import handlers
+        self.load_applications()
         super(App, self).__init__(handlers=self.handlers, **settings)
 
-    @classmethod
-    def add_handler(cls, hanlder: Type[BaseJsonRequestHandler]):
-        cls.handlers.append([hanlder.route, hanlder])
-        logger.info(f"Register Handler in path {hanlder.route}")
-        from devtools import debug
-        debug(f"Register Handler in path {hanlder.route}")
-
-        return hanlder
+    def load_applications(self):
+        for app in self.apps:
+            module = import_object(app)
+            application = module.application
+            application_class: Type[BaseApplication] = import_object(f"{app}.{application}")
+            routes = application_class().get_routes()
+            if routes:
+                from devtools import debug
+                self.handlers.extend(routes)
+            if application_class.models_path is not None:
+                self.models.append(f"{app}.{application_class.models_path}")
