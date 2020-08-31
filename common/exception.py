6c2ed356a5d99e171f9e27a2e205c40bd8a982f5
Artur Spatari <artur.spatari@gmail.com>
2020-08-31 18:13:19 +0300

    feat(core) Add DoseNotExist exception

--- common/exception.py
+++ common/exception.py
@@ -1,11 +1,18 @@
 from tornado.web import HTTPError
+from tortoise import Model
+
+from common.types import ID
 
 
 class TornadoValidationError(HTTPError):
-
     def __init__(
         self, status_code: int = 500, log_message: str = None, *args, **kwargs
     ) -> None:
         self.status_code = status_code
         self.log_message = log_message
 
+
+class TornadoDoseNotExist(HTTPError):
+    def __init__(self, id: ID, *args, **kwargs) -> None:
+        self.status_code = 404
+        self.log_message = f"Object with {id} Dose Not Exist"
