cc7e7c1f9e9bb7718e625a0d604218951886bb77
Artur Spatari <artur.spatari@gmail.com>
2020-08-31 18:14:07 +0300

    feat(client) Add retrieve and delete methods

--- apps/clients/handlers/client_detail.py
+++ apps/clients/handlers/client_detail.py
@@ -1,19 +1,29 @@
+from common.exception import TornadoDoseNotExist
 from common.handler import BaseJsonRequestHandler
 from common.types import ID
 from .. import serializers, service
+from tortoise.exceptions import DoesNotExist
 
 
 class ClientCreateEditDelete(BaseJsonRequestHandler):
     route = '/v1/clients/(?P<client_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})'
 
     async def get(self, client_id: ID):
-        client = await service.client_service.get_client(client=client_id)
-        schema = serializers.ClientDetailSchema()
-        result = schema.dump(client)
-        self.success(result)
+        try:
+            client = await service.client_service.get_client(client=client_id)
+            schema = serializers.ClientDetailSchema()
+            result = schema.dump(client)
+            self.success(result)
+        except DoesNotExist as e:
+            self.error(TornadoDoseNotExist(id=client_id))
 
     def patch(self, client_id: ID):
         self.finish({"method": "patch"})
 
-    def delete(self, client_id: ID):
-        self.finish({"method": "delete"})
+    async def delete(self, client_id: ID):
+        try:
+            client = await service.client_service.delete_client(client=client_id)
+            self.success({}, status=204)
+
+        except DoesNotExist as e:
+            self.error(TornadoDoseNotExist(id=client_id))
